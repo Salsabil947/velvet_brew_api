0af7a196fa41da3e61db4520c1ff1443cae3fa25
Salsabil947 <Salsabiledris821@gmail.com>
2026-05-02 14:52:21 +0300

    Add allowed origin for production and local development

--- config/cors.php
+++ config/cors.php
@@ -9,7 +9,8 @@ function set_cors_headers(): void {
     // 👇 Allowed frontend origins
     $allowed_origins = [
         'http://localhost:5173',
-        'http://localhost:5174'
+        'http://localhost:5174',
+        'https://velvet-brew-lac.vercel.app/'
     ];
 
     // 👇 Get request origin
